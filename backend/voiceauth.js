e3aca098b070eccb68707373ba2b649bbd1735f6
Ritwik <ritwikgoel@yahoo.com>
2021-09-05 14:22:05 +0530

    Fix: Main Structure

---
+++ backend/voiceauth.js
@@ -0,0 +1,15 @@
+const meyda= require("meyda");
+
+//own signal for the input 
+
+var signal = new Array(32).fill(0).map((element, index) => {
+    const remainder = index % 3;
+    if (remainder === 0) {
+      return 1;
+    } else if (remainder === 1) {
+      return 0;
+    }
+    return -1;
+  });
+//extraction of features
+  console.log(meyda.extract("zcr", signal));
