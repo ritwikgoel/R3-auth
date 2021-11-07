d5ff1e46a104e53f47c3bd2ac6321516db8d4dba
Ritwik Goel <ritwikgoel@yahoo.com>
2021-11-08 01:39:10 +0530

    Fix: small issues and project structure

--- frontend/voiceauth.js
+++ frontend/voiceauth.js
@@ -8,12 +8,12 @@ const meyda= require("meyda");
 var signal = new Array(32).fill(0).map((element, index) => {
     const remainder = index % 3;
     if (remainder === 0) {
-      return 1;
-    } else if (remainder === 1) {
       return 0;
+    } else if (remainder === 1) {
+      return 1;
     }
     return -1;
   });
 //extraction of features
-console.log(signal);
-console.log(meyda.extract("zcr", signal));
+//console.log(signal);
+console.log(meyda.extract("rms", signal));
