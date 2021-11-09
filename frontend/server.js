ad33ab69892dcfdd91d89ce47dc60d4773f3ea2e
Ritwik Goel <ritwikgoel@yahoo.com>
2021-11-09 18:05:22 +0530

    Fix: mongo connection

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,17 @@
 var express = require('express'),
     path = require('path'),
     app = express();
+    var bodyParser = require('body-parser')
+
     //console.log(path.join(__dirname, 'signup.html'))
     var mongoose = require("mongoose")
+    var jsonParser = bodyParser.json()
 
+    var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
     mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Raghavaro:banana_auth@bananacluster.lfdqb.mongodb.net/banasnadb?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Raghavaro:banana_auth@bananacluster.lfdqb.mongodb.net/bananadb?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
@@ -18,8 +22,8 @@ mongoose.connect('mongodb+srv://Raghavaro:banana_auth@bananacluster.lfdqb.mongod
   }
 });
 var Schema = new mongoose.Schema({
-  email: String,
-  password: String
+  name: String,
+  password: String,
 });
 var User=mongoose.model("User",Schema);
 
@@ -28,9 +32,17 @@ var User=mongoose.model("User",Schema);
     app.use('/', express.static(path.join(__dirname, '')))
     app.use('/signup', express.static(path.join(__dirname, 'signup.html')))
     app.use('/main', express.static(path.join(__dirname, 'main.html')))
-    app.post("/addname", (req, res) => {
-        var myData = new User(req.body);
-        console.log(myData);
+    app.post("/addname", urlencodedParser,(req, res) => {
+      console.log({
+        name: req.body.name,
+        password: req.body.pwd
+      });
+
+        var myData = new User({
+          name: req.body.name,
+          password: req.body.pwd
+        });
+        //console.log(myData);
         myData.save()
           .then(item => {
             res.send("item saved to database");
