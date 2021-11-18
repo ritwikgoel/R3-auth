eaaea83267da6381bd4e93d7244e5fc45a0a255d
Anitej Srivastava <68562793+codeup729@users.noreply.github.com>
2021-11-18 13:44:07 +0530

    Added login, signup, voice record

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     path = require('path'),
     app = express();
-    var bodyParser = require('body-parser')
-
+    var bodyParser = require('body-parser');
+const { callback } = require('meyda');
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
     //console.log(path.join(__dirname, 'signup.html'))
     var mongoose = require("mongoose")
     var jsonParser = bodyParser.json()
@@ -26,23 +28,72 @@ var Schema = new mongoose.Schema({
   password: String,
 });
 var User=mongoose.model("User",Schema);
-
+app.post('/adminpanelauthenticated',(req,res)=>{
+  User.find({}, function(err, users){
+    res.render('adminP',{
+      credentialslist:users
+    })
+  })
+})
 
 
     app.use('/', express.static(path.join(__dirname, '')))
     app.use('/signup', express.static(path.join(__dirname, 'signup.html')))
     app.use('/main', express.static(path.join(__dirname, 'main.html')))
     app.post("/addname", urlencodedParser,(req, res) => {
-      console.log({
+      const callback = (RESULT) => {
+        if(RESULT){
+          res.sendFile(__dirname+"/voice.html");
+          // res.send(RESULT);
+        }
+      }
+      var name = req.body.name;
+      var password = req.body.pwd;
+      console.log(name);
+      console.log(password);
+      User.findOne({name},(err,data)=>{
+        if(err || !data){
+          return callback(false);
+        }
+        if(data.password==password){
+          //res.send("success")
+          return callback(true);
+        }
+        else{
+          return callback(false);
+        }
+      });
+
+
+      // console.log({
+      //   name: req.body.name,
+      //   password: req.body.pwd
+      // });
+
+        // var myData = new User({
+        //   name: req.body.name,
+        //   password: req.body.pwd
+        // });
+      //   //console.log(myData);
+      //   myData.save()
+      //     .then(item => {
+      //       res.send("item saved to database");
+      //     })
+      //     .catch(err => {
+      //       res.status(400).send("unable to save to database");
+      //     });
+    });
+    app.post("/signing", urlencodedParser,(req, res) => {
+      var name = req.body.name;
+      var password1 = req.body.pwd;
+      var password2 = req.body.pwd2;
+      console.log(name);
+      console.log(password1);
+      var myData = new User({
         name: req.body.name,
         password: req.body.pwd
       });
-
-        var myData = new User({
-          name: req.body.name,
-          password: req.body.pwd
-        });
-        //console.log(myData);
+      if(password1==password2){
         myData.save()
           .then(item => {
             res.send("item saved to database");
@@ -50,7 +101,11 @@ var User=mongoose.model("User",Schema);
           .catch(err => {
             res.status(400).send("unable to save to database");
           });
-      });
+      }
+      else{
+        alert("Passwords not matching");
+      }
+    });
       
 
 app.listen(8080);
