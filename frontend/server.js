55b9279b2449eef4a4bfe8f0828b1aab9638b2ea
Ritwik Goel <ritwikgoel@yahoo.com>
2021-11-22 01:20:51 +0530

    Fix: The entire project

--- frontend/server.js
+++ frontend/server.js
@@ -56,13 +56,13 @@ app.post('/adminpanelauthenticated',(req,res)=>{
   }
   //console.log(data) 
   //data is here 
-  console.log(data.indexOf('Average zcr: '));
-  console.log(data.indexOf('Average zcr: '));  // Expected output: 9
-  console.log(data.substring(data.indexOf('Average zcr: ')+13,data.indexOf('Average zcr: ')+18))
-  console.log(data.substring(data.indexOf('Average rms: ')+13,data.indexOf('Average rms: ')+20))
-  console.log(data.substring(data.indexOf('Average energy: ')+16,data.indexOf('Average energy: ')+23))
+  //console.log(data.indexOf('Average zcr: '));
+  //console.log(data.indexOf('Average zcr: '));  // Expected output: 9
+  //console.log(data.substring(data.indexOf('Average zcr: ')+13,data.indexOf('Average zcr: ')+18))
+  //console.log(data.substring(data.indexOf('Average rms: ')+13,data.indexOf('Average rms: ')+20))
+  //console.log(data.substring(data.indexOf('Average energy: ')+16,data.indexOf('Average energy: ')+23))
   let keyValue=0.65*data.substring(data.indexOf('Average zcr: ')+13,data.indexOf('Average zcr: ')+18)+ 0.30*data.substring(data.indexOf('Average rms: ')+13,data.indexOf('Average rms: ')+20)+ 0.05*data.substring(data.indexOf('Average energy: ')+16,data.indexOf('Average energy: ')+23);
-  console.log(keyValue)
+  //console.log(keyValue)
   res.send({ key: keyValue });
 
 })
@@ -76,61 +76,64 @@ app.post('/adminpanelauthenticated',(req,res)=>{
     app.post("/addname", urlencodedParser,(req, res) => {
       const callback = (RESULT) => {
         if(RESULT){
-          res.sendFile(__dirname+"/voice.html");
+          res.sendFile(__dirname+"/success.html");
           // res.send(RESULT);
+        }
+        else{
+          res.sendFile(__dirname+"/fail.html");
+
         }
       }
       var name = req.body.name;
       var password = req.body.pwd;
-      var key=req.body.finalvalue;
-      console.log(name);
-      console.log(password);
-      console.log(key);
+      var key=req.body.keyfinal;
+      //console.log(name);
+      //console.log(password);
+      //console.log(key);//this is a string 
+      //we have to convert it to double 
+      //then convert data.key to double 
+      //then compare 
+      //then it should run 
+
       User.findOne({name},(err,data)=>{
         if(err || !data){
           return callback(false);
         }
-        if(data.password==password && data.key==key){
+        var keyfloat=parseFloat(key);
+        var datafloat=parseFloat(data.key);
+
+        //console.log(typeof(data))
+        //console.log(keyfloat)
+
+        //console.log(datafloat)
+        let racekeyfloat=keyfloat+(0.25*keyfloat);
+        let racekeyfloatminus=keyfloat-(0.25*keyfloat);
+        console.log(racekeyfloat)
+        console.log(racekeyfloatminus)
+
+        if(data.password==password && (racekeyfloatminus<=datafloat || datafloat>=racekeyfloat)){
           //res.send("success")
+          //console.log("Is this workingn")
           return callback(true);
         }
         else{
-          alert("Login failed")
+          console.log("Login failed")
           return callback(false);
         }
       });
-
-
-      // console.log({
-      //   name: req.body.name,
-      //   password: req.body.pwd
-      // });
-
-        // var myData = new User({
-        //   name: req.body.name,
-        //   password: req.body.pwd
-        // });
-      //   //console.log(myData);
-      //   myData.save()
-      //     .then(item => {
-      //       res.send("item saved to database");
-      //     })
-      //     .catch(err => {
-      //       res.status(400).send("unable to save to database");
-      //     });
     });
     app.post("/signing", urlencodedParser,(req, res) => {
       var name = req.body.name;
       var password1 = req.body.pwd;
       var password2 = req.body.pwd2;
-      var key= req.body.finalvalue;
+      var key=req.body.keyfinal;
       console.log(name);
       console.log(password1);
       console.log(key)
       var myData = new User({
         name: req.body.name,
         password: req.body.pwd,
-        key: req.body.finalvalue
+        key: req.body.keyfinal
       });
       if(password1==password2){
         myData.save()
